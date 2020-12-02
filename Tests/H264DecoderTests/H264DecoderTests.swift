f4d88d587b2a40f78ea93d0247cdde501716df5f
Unpxre <unpxre.tw@gmail.com>
2020-12-02 13:40:32 +0800

    [fix] 多餘測試程式碼

--- Tests/H264DecoderTests/H264DecoderTests.swift
+++ Tests/H264DecoderTests/H264DecoderTests.swift
@@ -6,11 +6,6 @@ final class H264DecoderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        var decoder = H264Decoder()
-        decoder.setHandler { _ in
-            
-        }
-        decoder.qnqueue(<#T##Raw H.264 Data##Data#>)
     }
 
     static var allTests = [
