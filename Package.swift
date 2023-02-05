cd1fd698f68cedf8a848bc7544ee2e866190b8b7
Unpxre <unpxre.tw@gmail.com>
2023-02-05 16:03:41 +0800

    feat: 增加編譯相關工具

--- Package.swift
+++ Package.swift
@@ -6,10 +6,28 @@ let package = Package(
     name: "H264Decoder",
     platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11)],
     products: [
-        .library(name: "H264Decoder", targets: ["H264Decoder"]),
+        .library(
+            name: "H264Decoder",
+            targets: ["H264Decoder"]
+        )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            url: "https://github.com/nicklockwood/SwiftFormat.git",
+            from: Version(0, 50, 8)
+        )
+    ],
     targets: [
-        .target(name: "H264Decoder", dependencies: []),
+        .target(
+            name: "H264Decoder",
+            plugins: [
+                "SwiftLintBinary"
+            ]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.50.3/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "abe7c0bb505d26c232b565c3b1b4a01a8d1a38d86846e788c4d02f0b1042a904"
+        )
     ]
 )
