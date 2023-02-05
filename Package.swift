c72df4a59f4819990b0ef2af243c085c0fbf808c
Unpxre <unpxre.tw@gmail.com>
2023-02-05 16:03:10 +0800

    feat: 更新版本要求

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "H264Decoder",
-    platforms: [.iOS(.v9), .macOS(.v10_11), .tvOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11)],
     products: [
         .library(name: "H264Decoder", targets: ["H264Decoder"]),
     ],
