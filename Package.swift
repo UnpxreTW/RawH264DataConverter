3d1ac6a98c3278c639e39dbe8ae4763162f0d3be
Unpxre <unpxre.tw@gmail.com>
2023-02-25 20:13:33 +0800

    chore: 專案目錄結構更改

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ let package = Package(
     name: "H264Decoder",
     platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11)],
     products: [
-        .library(
-            name: "H264Decoder",
-            targets: ["H264Decoder"]
-        )
+        .library(name: "H264Decoder", targets: ["H264Decoder"])
     ],
     dependencies: [
         .package(
@@ -20,14 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "H264Decoder",
-            plugins: [
-                "SwiftLintBinary"
-            ]
-        ),
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.50.3/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "abe7c0bb505d26c232b565c3b1b4a01a8d1a38d86846e788c4d02f0b1042a904"
+            path: "Sources"
         )
     ]
 )
